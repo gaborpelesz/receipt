8e8cdc68a7e97279759429aef34130d8d93baa81
Pelesz Gabor <gaborpelesz@gmail.com>
2020-04-14 22:06:20 +0200

    Implementing date and AP code extractor functions

--- app/receipt/processing.py
+++ app/receipt/processing.py
@@ -1,17 +1,47 @@
-import numpy as np
 import cv2
+import numpy as np
 import time
 import pytesseract
+import multiprocessing
+
+import config
+from utils.rotate import rotate
 
 class Receipt():
     def __init__(self, cropped_receipt):
+        self.date, self.AP = "?", "?"
         self.cropped_receipt = cropped_receipt
 
-    def get_AP(self):
-        pass
+    def get_date_and_AP(self):
 
-    def get_date(self):
-        pass
+        image = self.cropped_receipt[3*self.cropped_receipt.shape[0]//4:, :]
+        image_height, image_width = image.shape[:2]
+
+        text_images = self.find_text(image)
+
+        if len(text_images) == 0:
+            return "?", "?"
+
+        AP_candidate = text_images[0][0]
+        date_candidate = None
+
+        for text_image in text_images[1:]:
+            if text_image[1][0] < image_width//3:
+                date_candidate = text_image[0]
+                break
+
+        ocr_start = time.time()
+        self.AP = image_word_to_string(AP_candidate)
+
+        if date_candidate is not None:
+            self.date = image_word_to_string(date_candidate)
+        ocr_end = time.time()
+        print(f'     AP code: {self.AP}')
+        print(f'        date: {self.date}')
+        
+        print(f'ocr runtime: {(ocr_end-ocr_start)*1000:.2f}ms') # time
+        
+        return self.date, self.AP
 
     def get_all_text(self):
         img = self.cropped_receipt[3*self.cropped_receipt.shape[0]//4:, :]
@@ -34,24 +64,21 @@ class Receipt():
         with open('test.txt', 'w') as f:
             f.write(ocr_text)
 
-    def find_text(self, DEBUG=False):
-        img = self.cropped_receipt[3*self.cropped_receipt.shape[0]//4:, :]
+    def find_text(self, image):
+        im_height, im_width = image.shape[:2]
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (4, 4))
+
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
         grad = cv2.morphologyEx(gray, cv2.MORPH_GRADIENT, kernel)
 
 
         _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (35, 3))
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (31, 2))
         connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
 
-
-        # using RETR_EXTERNAL instead of RETR_CCOMP
         contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        #For opencv 3+ comment the previous line and uncomment the following line
-        #_, contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
         mask = np.zeros(bw.shape, dtype=np.uint8)
 
@@ -62,10 +89,10 @@ class Receipt():
             cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
             r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
 
-            if r > 0.45 and w > 16 and h > 16:
+            if r > 0.30 and w/im_width > 0.08 and h > 16:
                 rect = cv2.minAreaRect(contours[idx])
 
-                text_mask = np.zeros(img.shape[:2], dtype=np.uint8)
+                text_mask = np.zeros(image.shape[:2], dtype=np.uint8)
 
                 convex_hull = cv2.convexHull(contours[idx])
                 cv2.drawContours(text_mask, [convex_hull], -1, 255, -1)
@@ -75,10 +102,10 @@ class Receipt():
 
         text_images = []
         smoothed_image = None
-        for text_box in text_boxes[:10]:
+        for text_box in text_boxes:
             angle = -text_box[0][2] if text_box[0][2] > -45 else 270-text_box[0][2]
 
-            rotated_image, rotated_mask = rotate([img, text_box[1]], angle, center=text_box[0][0])
+            rotated_image, rotated_mask = rotate([image, text_box[1]], angle, center=text_box[0][0])
 
             contour = cv2.findContours(rotated_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0][0]
             x, y, w, h = cv2.boundingRect(contour)
@@ -88,46 +115,41 @@ class Receipt():
             smoothed_image = cv2.bitwise_and(rotated_image[y:y+h,x:x+w], rotated_mask)
             smoothed_image = cv2.add(smoothed_image, cv2.bitwise_not(rotated_mask))
             
-            text_images.append(smoothed_image)
+            position = (text_box[0][0][0], text_box[0][0][1]) # text center position on the image
 
-        for i, text_box in enumerate(text_boxes):
-            box = cv2.boxPoints(text_box[0])
-            box = np.int0(box)
+            text_images.append((smoothed_image, position))
 
-            img = cv2.putText(img, f'{i+1}', tuple(box[0]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
-            cv2.drawContours(img,[box],0,(0,0,255),2)
 
-        if DEBUG:
-            if smoothed_image is not None:
-                cv2.namedWindow('rotated', cv2.WINDOW_NORMAL)
-                cv2.resizeWindow('rotated', (1500,1500))
-                cv2.imshow('rotated', smoothed_image)
+        if config.DEBUG:
+            for i, text_box in enumerate(text_boxes):
+                box = cv2.boxPoints(text_box[0])
+                box = np.int0(box)
 
+                image = cv2.putText(image, f'{i+1}', tuple(box[0]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2, cv2.LINE_AA)
+                cv2.drawContours(image,[box],0,(0,0,255),2)
+
+            cv2.namedWindow('text boxes', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('text boxes', 1200,1000)
+            cv2.imshow('text boxes', self.cropped_receipt)
 
             cv2.namedWindow('grad', cv2.WINDOW_NORMAL)
             cv2.resizeWindow('grad', 1200,1000)
             cv2.imshow('grad', grad)
+
+            cv2.namedWindow('otzu', cv2.WINDOW_NORMAL)
+            cv2.resizeWindow('otzu', 1200,1000)
+            cv2.imshow('otzu', bw)
+
             cv2.namedWindow('connected', cv2.WINDOW_NORMAL)
             cv2.resizeWindow('connected', 1200,1000)
             cv2.imshow('connected', connected)
-            cv2.namedWindow('show', cv2.WINDOW_NORMAL)
-            cv2.resizeWindow('show', (1500,1500))
-            cv2.imshow('show', self.cropped_receipt)
-        return text_images, img
+        return text_images
 
+def smooth(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    adaptive = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 1)
+    return adaptive
 
-def rotate(images, angle, center=None):
-    if center is None:
-        center = (images[0].shape[1]//2, images[0].shape[0]//2)
-
-    (h, w) = images[0].shape[:2]
-    M = cv2.getRotationMatrix2D(center, -angle, 1.0)
-    cos = np.abs(M[0, 0])
-    sin = np.abs(M[0, 1])
-    nW = int((h * sin) + (w * cos))
-    nH = int((h * cos) + (w * sin))
-    M[0, 2] += (nW / 2) - center[0]
-    M[1, 2] += (nH / 2) - center[1]
-
-    # perform the actual rotation and return the images
-    return [cv2.warpAffine(image, M, (nW, nH)) for image in images]
+def image_word_to_string(text_image):
+    tesseract_single_word_config = '--psm 8 --oem 1'
+    return pytesseract.image_to_string(text_image, config=tesseract_single_word_config)
