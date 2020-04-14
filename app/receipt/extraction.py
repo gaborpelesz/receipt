b72b758249f86a4fefe9a9efa909600659f65b99
Pelesz Gabor <gaborpelesz@gmail.com>
2020-04-14 22:05:55 +0200

    Refactor and minor changes to extraction class

--- app/receipt/extraction.py
+++ app/receipt/extraction.py
@@ -8,10 +8,10 @@ from keras_segmentation.data_utils.data_loader import get_image_arr
 from keras_segmentation.models.config import IMAGE_ORDERING
 
 from receipt.processing import Receipt
+from utils.rotate import rotate
 
 class ReceiptExtractor:
     def __init__(self, segmentation_model_path='/home/peleszgabor/Desktop/projects/blokkos/alpha1.0/app/models/segmentation/resnet50_unet_20200318', use_gpu=False):
-        # TODO warmup neural network, if not the first prediction will be slow
         gpus = tf.config.experimental.list_physical_devices('GPU')
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
@@ -20,6 +20,8 @@ class ReceiptExtractor:
             tf.config.experimental.set_visible_devices([], 'GPU') # forces operations on CPU
 
         self.model = model_from_checkpoint_path(segmentation_model_path)
+
+        # warmup neural network, if not, the first prediction would be slow
         self._warmup()
 
     def _warmup(self):
@@ -33,18 +35,18 @@ class ReceiptExtractor:
         # Convert image to gray and then resize it for performance
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         resized_image = cv2.resize(gray, (400,400))
+
         # threshold for better white extraction (correct scanner machine errors and lightning)
         _, resized_image = cv2.threshold(resized_image, 220, 255, cv2.THRESH_BINARY)
 
         all_pixels = resized_image.shape[0] * resized_image.shape[1]
 
-        # create an array with True values where the images contained a 255
-        # then test whether all array elements along a 
-        # given axis (pixelwise in this case) evaluate to True
+        # find and add up the white pixels
         white_pixels = np.sum(resized_image==255)
 
         # if white pixels are above 80% of the image
         # we consider it as a scanned image
+        # (value is picked after testing)
         if white_pixels / all_pixels > 0.8:
             return True
         else:
@@ -56,20 +58,26 @@ class ReceiptExtractor:
         cropped_receipt = None
 
         if self.is_scanned(image):
-            cropped_receipt = self.extract_by_text(image)
+            print("The image looks like it was scanned.")
+            print("Text search based receipt extraction begin...")
+            cropped_receipt = self._extract_by_text_search(image)
         else:
-            cropped_receipt = self.extract_by_segment(image)
+            t0 = time.time()
+            print("Receipt segmentation with U-net begins...")
+            cropped_receipt = self._extract_by_segment(image)
+            print(f"Finished segmentation. ({(time.time()-t0)*1000:.2f}ms)")
 
         return Receipt(cropped_receipt) # create a new Receipt object
 
-    def extract_by_segment(self, image):
+    def _extract_by_segment(self, image):
         """Extraction based on the segmented image. Segmentation
         aims to segment the hole receipt on the image. At this stage
-        we assume that the image truly contains a receipt.
+        we assume that the image truly contains a receipt, with a distinct
+        background.
         """
         t_start = time.time()
 
-        segmentation_mask = self.segment_image(image)
+        segmentation_mask = self._segment_image(image)
 
         contours, _ = cv2.findContours(segmentation_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         # Our best guess here is to operate with the biggest contour later on
@@ -96,8 +104,8 @@ class ReceiptExtractor:
             smoothed_image = rotate([smoothed_image], 90)[0]
 
 
+        # TODO remove debug code below
         # print(f'Runtime: {(time.time()-t_start)*1000:.3f}ms')
-
         # window_size = (1500,1500)
         # cv2.namedWindow('show', cv2.WINDOW_NORMAL)
         # cv2.resizeWindow('show', window_size)
@@ -108,24 +116,28 @@ class ReceiptExtractor:
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
 
-        return Receipt(smoothed_image)
+        return smoothed_image
 
 
 
-    def segment_image(self, image_to_segment):
+    def _segment_image(self, image_to_segment):
         original_height, original_width = image_to_segment.shape[0], image_to_segment.shape[1] 
 
         # Making the prediction over the image with the trained model
         pr = predict(model=self.model, image=image_to_segment)
 
+        # Transform prediction
         seg_img = np.zeros((240, 176), dtype=np.uint8)
         seg_img[:, :] += ((pr == 1)*(255)).astype('uint8')
+
+        # Upscale
         seg_img = cv2.resize(seg_img, (original_width, original_height))
 
         return seg_img
 
-    def extract_by_text(self):
-        pass
+    def _extract_by_text_search(self):
+        # TODO implement text extraction
+        raise NotImplementedError
 
     # TODO not sure perspective transform is needed yet
     def perspective_transform(self):
@@ -152,21 +164,3 @@ def predict(model=None , image=None , out_fname=None):
     pr = pr.reshape(( output_height ,  output_width , n_classes ) ).argmax( axis=2 )
 
     return pr
-
-
-def rotate(images, angle, center=None):
-    if center is None:
-        center = (images[0].shape[1]//2, images[0].shape[0]//2)
-
-    (h, w) = images[0].shape[:2]
-    M = cv2.getRotationMatrix2D(center, -angle, 1.0)
-    M_inv = cv2.getRotationMatrix2D(center, angle, 1.0)
-    cos = np.abs(M[0, 0])
-    sin = np.abs(M[0, 1])
-    nW = int((h * sin) + (w * cos))
-    nH = int((h * cos) + (w * sin))
-    M[0, 2] += (nW / 2) - center[0]
-    M[1, 2] += (nH / 2) - center[1]
-
-    # perform the actual rotation and return the images
-    return [cv2.warpAffine(image, M, (nW, nH)) for image in images]
