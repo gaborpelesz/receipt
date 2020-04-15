29c74b7e5c816122f3a2b90b4c53d1519b25e8bb
Pelesz Gabor <gaborpelesz@gmail.com>
2020-04-15 21:40:07 +0200

    Create API

--- app/app.py
+++ app/app.py
@@ -1,25 +1,65 @@
 import time
-import json
-from dotenv import load_dotenv
-from flask import Flask, request, redirect, jsonify
+import io
+from flask import Flask, request, jsonify
 from flask_api import FlaskAPI, status
+import cv2
+import numpy as np
+
+from receipt.extraction import ReceiptExtractor
+import config
+from utils import base64_image_converter
 
 app = FlaskAPI(__name__)
+extractor = ReceiptExtractor(use_gpu=config.GPU)
 
-
-@app.route('/process_receipt', methods=['POST'])
-def hello_world():
-    if request.method == 'POST' and request.is_json:
-        return {
-                'TODO': 'Hello world!'
-            }, status.HTTP_500_INTERNAL_SERVER_ERROR
-        
+@app.route('/process_receipt', methods=['POST'])     
 def process_receipt():
-    receipt = extractor.extract_receipt(img)
-    ap_code = receipt.get_AP()
-    date = receipt.get_date()
+    if request.method == 'POST' and request.files.get('image'):
+        start_time = time.time() # track runtime
+
+        print('Decoding bytes to image...')
+        t0_decode = time.time()
+        image_bytes = request.files['image'].read()
+        image = cv2.imdecode(np.frombuffer(image_bytes, np.uint8), -1)
+        print(f'Decoding completed. ({(time.time()-t0_decode)*1000:.2f}ms)')
+
+
+
+        print('Extracting receipt from image...')
+        t0_extraction = time.time()
+        receipt = extractor.extract_receipt(image)
+        print(f'Extraction completed. ({(time.time()-t0_extraction)*1000:.2f}ms)')
+
+        if receipt is None:
+            return jsonify(
+                status='Success',
+                receipt={
+                    'date': '?',
+                    'AP_code': '?'
+                },
+                runtime=f'{(time.time()-start_time)*1000:.2f}ms'
+            ), status.HTTP_404_NOT_FOUND
+
+        print('Start OCR on receipt...')
+        t0_ocr = time.time()
+        date, AP = receipt.get_date_and_AP()
+        print(f'OCR completed. ({(time.time()-t0_ocr)*1000:.2f}ms)')
+
+        return jsonify(
+            status='Success',
+            receipt={
+                'date': date,
+                'AP_code': AP,
+            },
+            runtime=f'{(time.time()-start_time)*1000:.2f}ms'
+        ), status.HTTP_200_OK
+
+    return jsonify(
+        status='Failed',
+        status_message='The request method was not "POST".' if request.method != 'POST' else 'The request is not a JSON request.'
+    ), status.HTTP_400_BAD_REQUEST
 
 
 if __name__ == '__main__':
-    load_dotenv(override=True)
-    app.run(host='0.0.0.0', port=3000)
+    # threaded=false because if not then tensorflow would cry
+    app.run(host='0.0.0.0', port=3000, threaded=False)
