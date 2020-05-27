1147cc77bec044bc1d617e2d0f06819186375ab1
Pelesz Gabor <gaborpelesz@hotmail.com>
2020-05-27 17:23:09 +0200

    Refactor code structure

---
+++ app/receipt/text/ocr.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import pytesseract
+
+import config
+from utils.rotate import rotate
+
+def image_word_to_string(text_image):
+    tesseract_single_word_config = '--psm 13 --oem 1'
+    return pytesseract.image_to_string(text_image, config=tesseract_single_word_config)
